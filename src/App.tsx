81d7e724716cef7ce36e4b6faaff6581fcb771c6
comfyrandom <comfyrandom@gmail.com>
2025-12-23 19:28:08 +0300

    Removed auth (temporary)

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import Layout from './components/Layout.tsx';
 import Users from "./Pages/Users.tsx";
 import Analytics from "./Pages/Analytics.tsx";
 import Settings from "./Pages/Settings.tsx";
-import {AuthProvider} from "./contexts/AuthContext.tsx";
-import LoginPage from "./Pages/LoginPage.tsx";
 
 function App() {
     return (
-        <AuthProvider>
         <Router>
             <Layout>
                 <Routes>
@@ -17,11 +14,9 @@ function App() {
                     <Route path="/settings" element={<Settings />} />
                     <Route path="/users" element={<Users />} />
                     <Route path="/analytics" element={<Analytics />} />
-                    <Route path="/login" element={<LoginPage />} />
                 </Routes>
             </Layout>
         </Router>
-        </AuthProvider>
     );
 }
 
